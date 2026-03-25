518629d7e464931223cd35b4b3f2ba7f75083188
dv-jamie <sjm2506@gmail.com>
2026-03-25 18:55:30 +0900

    라우팅 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import ContactTest from './ContactTest'
-import Archive from './pages/Archive'; // 실제 자료실 페이지
-import ProtectedRoute from './components/ProtectedRoute';
+import Home from './Home'; // 실제 자료실 페이지
+import LoginPage from './LoginPage'; // 실제 자료실 페이지
+import Archive from './Archive'; // 실제 자료실 페이지
+import ProtectedRoute from './ProtectedRoute';
 import './App.css'
 
 function App() {
   return (
     <>
-      <section id="center">
-        <div>
-          <h1>사이트 준비 중입니다.</h1>
-        </div>
-      </section>
-
-      <ContactTest />
-
       <BrowserRouter>
         <Routes>
           {/* 누구나 볼 수 있는 일반 페이지 */}
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<LoginPage />} />
+          
 
           {/* 🔒 보호되는 페이지: ProtectedRoute로 감쌉니다. */}
           <Route 
---
+++ src/Home.jsx
@@ -0,0 +1,15 @@
+import ContactTest from "./ContactTest"
+
+function Home() {
+    return (
+        <section id="center">
+            <div>
+                <h1>사이트 준비 중입니다.</h1>
+            </div>
+
+            <ContactTest />
+        </section>
+    )
+}
+
+export default Home
